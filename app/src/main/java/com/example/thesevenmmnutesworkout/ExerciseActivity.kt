0190afb9c8c2fcd35f8c11e2d9c62a92549a992e
Emad-eldin-Teama <eng.emadeldin.teama@gmail.com>
2020-12-25 00:27:26 +0200

    git init

--- app/src/main/java/com/example/thesevenmmnutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/thesevenmmnutesworkout/ExerciseActivity.kt
@@ -3,25 +3,32 @@ package com.example.thesevenmmnutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_exercise.*
 
 class ExerciseActivity : AppCompatActivity() {
-    private var restTimer : CountDownTimer? = null
+    private var restTimer: CountDownTimer? = null
     private var restProgress = 0
-
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
+    private var exerciseList: ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
-    setSupportActionBar(toolbar_exercise_activity)
+        setSupportActionBar(toolbar_exercise_activity)
         val actionbar = supportActionBar
         actionbar?.setDisplayHomeAsUpEnabled(true)
         toolbar_exercise_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        exerciseList = Constants.defaultExerciseList()
         setupRestView()
+
     }
 
     override fun onDestroy() {
@@ -31,9 +38,10 @@ class ExerciseActivity : AppCompatActivity() {
         }
         super.onDestroy()
     }
-    private fun setRestProgressBar(){
+
+    private fun setRestProgressBar() {
         progressBar.progress = restProgress
-        restTimer = object :CountDownTimer(10000,1000) {
+        restTimer = object : CountDownTimer(10000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 restProgress++
                 progressBar.progress = 10 - restProgress
@@ -41,19 +49,60 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity,
-                        "Now we will start the exercise",
-                        Toast.LENGTH_SHORT).show()
+                currentExercisePosition++
+                setupExerciseView()
             }
-        }.start()}
-private fun setupRestView(){
-    if(restTimer!= null){
-        restTimer!!.cancel()
-        restProgress = 0
+        }.start()
     }
-    setRestProgressBar()
+
+    private fun setExerciseProgressBar() {
+        progressBarExercise.progress = exerciseProgress
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                progressBarExercise.progress = 30 - exerciseProgress
+                tvExerciseTimer.text = (30 - exerciseProgress).toString()
             }
 
+            override fun onFinish() {
+                if (currentExercisePosition < (exerciseList?.size!! - 1)) {
+                    setupRestView()
+                } else {
+                    Toast.makeText(this@ExerciseActivity, "Congratulations! you finished the workout", Toast.LENGTH_LONG).show()
+
+                }
+            }
+        }.start()
+    }
+
+    private fun setupRestView() {
+        llRestView.visibility = View.VISIBLE
+        llExerciseView.visibility = View.GONE
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
         }
+        tvUpcomingExerciseName.text = exerciseList!![currentExercisePosition + 1].getName()
+
+        setRestProgressBar()
+    }
+
+    private fun setupExerciseView() {
+        llRestView.visibility = View.GONE
+        llExerciseView.visibility = View.VISIBLE
+
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+
+
+        ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
+        setExerciseProgressBar()
+    }
+
+
+}
 
 
