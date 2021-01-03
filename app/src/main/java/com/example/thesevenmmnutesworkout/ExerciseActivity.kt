372d4cf9167c9123f604c6f09d53f4fa206949c1
Emad-eldin-Teama <eng.emadeldin.teama@gmail.com>
2021-01-03 16:23:39 +0200

    git init

--- app/src/main/java/com/example/thesevenmmnutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/thesevenmmnutesworkout/ExerciseActivity.kt
@@ -1,19 +1,31 @@
 package com.example.thesevenmmnutesworkout
 
+import android.media.MediaPlayer
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_exercise.*
+import org.w3c.dom.Text
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
     private var exerciseTimer: CountDownTimer? = null
     private var exerciseProgress = 0
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
+    private var tts:TextToSpeech?= null
+    private var player: MediaPlayer? = null
+    private var exerciseAdaptor :ExerciseStatusAdaptor? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +37,10 @@ class ExerciseActivity : AppCompatActivity() {
         toolbar_exercise_activity.setNavigationOnClickListener {
             onBackPressed()
         }
-
+            tts= TextToSpeech(this,this)
         exerciseList = Constants.defaultExerciseList()
         setupRestView()
+        setupExerciseStatusRecyclerView ()
 
     }
 
@@ -36,6 +49,17 @@ class ExerciseActivity : AppCompatActivity() {
             restTimer!!.cancel()
             restProgress = 0
         }
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+        if(tts!= null){
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        if(player!= null)
+            player!!.stop()
+
         super.onDestroy()
     }
 
@@ -48,8 +72,11 @@ class ExerciseActivity : AppCompatActivity() {
                 tvTimer.text = (10 - restProgress).toString()
             }
 
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
             override fun onFinish() {
                 currentExercisePosition++
+                exerciseList!![currentExercisePosition].setIsSelected(true)
+                exerciseAdaptor!!.notifyDataSetChanged()
                 setupExerciseView()
             }
         }.start()
@@ -65,7 +92,11 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+
                 if (currentExercisePosition < (exerciseList?.size!! - 1)) {
+                    exerciseList!![currentExercisePosition].setIsSelected(false)
+                    exerciseList!![currentExercisePosition].setIsCompleted(true)
+                    exerciseAdaptor!!.notifyDataSetChanged()
                     setupRestView()
                 } else {
                     Toast.makeText(this@ExerciseActivity, "Congratulations! you finished the workout", Toast.LENGTH_LONG).show()
@@ -76,6 +107,15 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setupRestView() {
+       try {
+           player = MediaPlayer.create(applicationContext,R.raw.press_start)
+           player!!.isLooping=false
+           player!!.start()
+       }
+       catch(e:Exception){
+           e.printStackTrace()
+       }
+
         llRestView.visibility = View.VISIBLE
         llExerciseView.visibility = View.GONE
         if (restTimer != null) {
@@ -87,6 +127,7 @@ class ExerciseActivity : AppCompatActivity() {
         setRestProgressBar()
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     private fun setupExerciseView() {
         llRestView.visibility = View.GONE
         llExerciseView.visibility = View.VISIBLE
@@ -96,12 +137,32 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseProgress = 0
         }
 
-
+        speakOut(exerciseList!![currentExercisePosition].getName())
+        setExerciseProgressBar()
         ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
         tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
-        setExerciseProgressBar()
+
     }
 
+    override fun onInit(status: Int) {
+    if (status == TextToSpeech.SUCCESS){
+        val result = tts!!.setLanguage(Locale.US)
+        if(result == TextToSpeech.LANG_MISSING_DATA|| result == TextToSpeech.LANG_NOT_SUPPORTED)
+    Log.e("TTS","the language specified is not supported")
+    }else{
+      Log.e("TTS","Text to speech failed!")
+    }
+    }
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun speakOut (text:String){
+        tts!!.speak(text,TextToSpeech.QUEUE_FLUSH,null,"")
+    }
+ private fun setupExerciseStatusRecyclerView (){
+     rvExerciseStatus.layoutManager = LinearLayoutManager(this,
+             LinearLayoutManager.HORIZONTAL,false)
+     exerciseAdaptor = ExerciseStatusAdaptor(exerciseList!!,this)
+     rvExerciseStatus.adapter = exerciseAdaptor
+ }
 
 }
 
