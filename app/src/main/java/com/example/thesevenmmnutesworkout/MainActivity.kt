51ec2f9ff8668ff8aabaceef5027fd16729cb435
Emad-eldin-Teama <eng.emadeldin.teama@gmail.com>
2021-01-10 10:17:34 +0200

    $ git push

--- app/src/main/java/com/example/thesevenmmnutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/thesevenmmnutesworkout/MainActivity.kt
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        llStart.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+
+        llBMI.setOnClickListener{
+            val intent = Intent (this,BMIActivity::class.java)
+         startActivity(intent)
+        }
+
 
     }
-    fun startButton(view:View){
-      val intent = Intent(this , ExerciseActivity::class.java)
-        startActivity(intent)
 
-    }
+
     }
